ebd1ebc0c5f05acb03b34b87fe398372f2464590
tasnim-jawad <muhammadtasnimulhasan1@gmail.com>
2024-06-13 17:56:51 +0600

    change

--- resources/js/Frontend/Pages/About/Index.vue
+++ resources/js/Frontend/Pages/About/Index.vue
@@ -1,10 +1,10 @@
 <template>
     <div>
-        <h1>About / index</h1>
+        <h1>About / index gdefjgdjfksgjks</h1>
     </div>
 </template>
 
 <script>
     export default {
     }
-</script>
+</script>s
